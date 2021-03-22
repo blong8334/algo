0ede53bdb2d6457efdff31e75c34c076a3e311f5
Brian Long <blong8334@gmail.com>
2021-03-22 07:43:57 -0400

    added random partition

--- sorting/quick-sort.js
+++ sorting/quick-sort.js
@@ -4,6 +4,8 @@ function quickSort(arr, lowerIdx = 0, upperIdx = arr.length - 1) {
   if (upperIdx <= lowerIdx) return;
   let partIdx = lowerIdx;
   let upper = upperIdx;
+  const newPart = Math.floor(Math.random() * (upperIdx - lowerIdx + 1) + lowerIdx);
+  swap(arr, partIdx, newPart);
   while (true) {
     if (upperIdx === partIdx) {
       quickSort(arr, lowerIdx, partIdx - 1);
