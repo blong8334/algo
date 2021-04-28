3930d543186b16c1a1324c234746124822d118ed
Brian Long <blong8334@gmail.com>
2021-04-28 08:21:39 -0400

    Added mst fn

--- lib/cpp-practice/test.cpp
+++ lib/cpp-practice/test.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 
+int* makeArray(long size){
+  int* boot = (int*)malloc((size + 1) * sizeof(int));
+  boot[size] = '\0';
+  return boot;
+}
+
+void printVar(int* boot, std::string name){
+  std::cout
+    << name << ": " << boot
+    << ", &" << name << ": " << &boot
+    << ", *" << name << ": " << *boot
+    << std::endl;
+}
+void printVar(int boot, std::string name){
+  std::cout
+    << name << ": " << boot
+    << ", &" << name << ": " << &boot
+    << std::endl;
+}
+
+struct BootHawk {
+  int maggot;
+};
+
 int main(){
-  int i = 10;
-  std::string boot = "Hello!";
-  std::cout << boot << std::endl;
+  BootHawk mule;
+  mule.maggot = 4;
+  std::cout << mule.maggot << std::endl;
+  // std::cout << **p2 << std::endl;
   return 0;
 }
